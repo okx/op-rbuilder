c8e8ce990ccc7a8316ab83380dc0a098ee2687ba
Niven <sieniven@gmail.com>
2026-02-10 14:48:33 +0800

    Switch async execute (#81)

--- crates/op-rbuilder/src/builders/flashblocks/payload.rs
+++ crates/op-rbuilder/src/builders/flashblocks/payload.rs
@@ -1008,7 +1008,7 @@ where
                                     fallback_payload_for_resolve
                                 })
                         } else {
-                            self.task_executor.spawn(Box::pin(async move {
+                            self.task_executor.spawn_blocking(Box::pin(async move {
                                 let _ = resolve_zero_state_root(state_root_ctx, state_provider);
                             }));
                             fallback_payload_for_resolve
